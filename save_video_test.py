beca12d1f33361a03540c58fd0b9fad15f108e71
casperfrc <casperfrc@gmail.com>
2023-12-14 22:59:42 +0100

    *WIP* Added a simple NN not working

--- save_video_test.py
+++ save_video_test.py
@@ -9,20 +9,25 @@ _ = env.reset()
 step_starting_index = 0
 episode_index = 0
 
-for step_index in range(199): 
+for step_index in range(10): 
    action = env.action_space.sample()
+
    _, _, terminated, truncated, _ = env.step(action)
 
-   if terminated or truncated:
-      save_video(
-         env.render(),
-         f"{time.strftime("%Y-%m-%d-%H-%M")}/videos",
-         fps=env.metadata["render_fps"],
-         step_starting_index=step_starting_index,
-         episode_index=episode_index
-      )
-      step_starting_index = step_index + 1
-      episode_index += 1
-      env.reset()
+   obs_space = env.observation_space
+   action_space = env.action_space
+   print("The observation space: {}".format(obs_space[0]))
+   print("The action space: {}".format(action_space))
+
+   # save_video(
+   #    env.render(),
+   #    f"{time.strftime('%Y-%m-%d-%H-%M')}/videos",
+   #    fps=env.metadata["render_fps"],
+   #    step_starting_index=step_starting_index,
+   #    episode_index=episode_index
+   # )
+   step_starting_index = step_index + 1
+   episode_index += 1
+   print(env.reset())
 
 env.close()
