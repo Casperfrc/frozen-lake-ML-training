9400a742b9f781fe79c7517b267af443a11b3f58
casperfrc <casperfrc@gmail.com>
2024-02-26 18:49:37 +0100

    Add some of the functionality environment step function

--- project_from_scratch/frozen_lake_environment.py
+++ project_from_scratch/frozen_lake_environment.py
@@ -1,31 +1,41 @@
-import gym
 import numpy as np
 import random
 
-import torch
-from torch import nn
-import torch.optim as optim
-
-
 from stolen_implementation.neural_network import NeuralNetwork
+
+# Use this to call:
+# python -m project_from_scratch.frozen_lake_environment.py
 
 class FrozenLake():
     def __init__(self, size):
-        frozenLakeHelper = FrozenLakeHelper()
-        self.grid = frozenLakeHelper.generate_grid(size)
-
-        env = gym.make('FrozenLake-v1', render_mode="rgb_array_list")
-        model = NeuralNetwork(env)
-
-        print(nn.Parameter())
-        print(model.parameters())
+        # Left, right, up, down
+        self.possible_actions = [1, 2, 3, 4]
+        self.size = size
+        self.frozenLakeHelper = FrozenLakeHelper()
+        self.grid = self.frozenLakeHelper.generate_grid(self.size)
    
     # Returns observation/state 
     def reset(self):
-        ""
+        self.grid, self.player_position, self.goal_position = self.frozenLakeHelper.generate_grid(self.size)
 
     # Returns observation/state
-    def step(self):
+    def step(self, action):
+        if action not in self.possible_actions:
+            return self.grid
+        
+        match action:
+            case 1: # Going left
+                # Checking if the player is at the edge
+                if self.player_position[1] == 0:
+                    return self.grid
+            case 2: # Going right
+                ""
+            case 3: # Going up
+                if self.player_position[0] == 0:
+                    return self.grid
+            case 4: # Going down
+                ""
+
         ""
 
     def print_grid(self):
@@ -56,12 +66,12 @@ class FrozenLakeHelper():
         grid = np.zeros((size, size), dtype=int)
 
         # Set player position
-        player_position = random.randint(0, size-1)
-        grid[0][player_position] = 1
+        player_x_position = random.randint(0, size-1)
+        grid[0][player_x_position] = 1
 
         # Set goal position
-        goal_position = random.randint(0, size-1)
-        grid[3][goal_position] = 3
+        goal_x_position = random.randint(0, size-1)
+        grid[3][goal_x_position] = 3
 
         for _ in range(size):
             position_not_player_or_goal = True
@@ -71,16 +81,17 @@ class FrozenLakeHelper():
                 random_column = random.randint(0, size-1)
 
                 # Check if broken lake piece is on top of player or goal
-                if (random_row == 0 and random_column == player_position or random_row == size -1 and random_column == goal_position):
+                if (random_row == 0 and random_column == player_x_position or random_row == size -1 and random_column == goal_x_position):
                     continue
 
-                # There is no check for if the problem is actually solvable
+                # There is no check for if the environment is actually solvable
                 
                 position_not_player_or_goal = False
 
             grid[random_row, random_column] = 2
         
-        return grid
+        # Hardcoding y for both goal and player
+        return grid, (player_x_position, 0), (goal_x_position, size-1)
 
 
 model = FrozenLake(4)
